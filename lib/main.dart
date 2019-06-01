9b160966ebaf989950d1af4f259f8934a5238e67
DevarshRanpara <devarshranpara0803@gmail.com>
2019-06-01 12:08:46 +0530

    Added custom tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hack2k19/Views/login_view.dart';
+import 'package:hack2k19/Views/temp.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: Temp(),
     );
   }
 }
